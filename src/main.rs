1cd0fc40bb1cfd736b082d3fc47d26c8c41f38b2
Dominique Chin-Jones <djones@rapptrlabs.com>
2024-11-13 15:58:46 -0500

    update metadata; crate name

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use std::{env, process};
 
-use minigrep::Config;
+use cmdcjones_minigrep::Config;
 
 fn main() {
     let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
-    if let Err(err) = minigrep::run(config) {
+    if let Err(err) = cmdcjones_minigrep::run(config) {
         eprintln!("Application error: {}", err);
         process::exit(1);
     }
